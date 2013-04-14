4bd164e2ee0bc0b970dd3c21d0358103838ea38d
Tagaki Aki <a-tagaki@esm.co.jp>
2013-04-14 20:42:52 +0900

    Refactor baseball.rb

--- baseball.rb
+++ baseball.rb
@@ -1,6 +1,3 @@
-top     = {field: '表', point: 0}
-bottom  = {field: '裏', point: 0}
-
 class String
   def numeral?
     Integer(self)
@@ -10,30 +7,21 @@ class String
   end
 end
 
-def play_game(top, bottom)
-  quit_flg = false
+def play_game(points)
+  top_point, bottom_point = [], []
 
-  9.times do |i|
-    [top, bottom].each do |team|
-      print "#{i+1}回#{team[:field]}："
-
-      inputted = gets.chomp
-
-      if inputted.numeral?
-        team[:point] += inputted.to_i
-      elsif inputted == 'quit'
-        quit_flg = true
-        break
-      else
-        puts 'oh(∵)数字を入力してください。'
-        redo
-      end
-    end
-    break if quit_flg
+  points.each_with_index do |point, i|
+    team = i.even? ? top_point : bottom_point
+    team << point
   end
+
+  [top_point.inject(:+), bottom_point.inject(:+)]
 end
 
 def display_result(top_point, bottom_point)
+  top_point    = 0 if top_point.nil?
+  bottom_point = 0 if bottom_point.nil?
+
   puts "【試合結果】"
   puts "#{top_point} 対 #{bottom_point} で"
 
@@ -46,5 +34,32 @@ def display_result(top_point, bottom_point)
   end
 end
 
-play_game(top, bottom)
-display_result(top[:point], bottom[:point])
+if $0 == __FILE__
+  top      = {field: '表', point: 0}
+  bottom   = {field: '裏', point: 0}
+
+  points   = []
+  quit_flg = false
+
+  9.times do |i|
+    [top, bottom].each do |team|
+      print "#{i+1}回#{team[:field]}："
+
+      inputted = gets.chomp
+
+      if inputted.numeral?
+        points << inputted.to_i
+      elsif inputted == 'quit'
+        quit_flg = true
+        break
+      else
+        puts 'oh(∵)数字を入力してください。'
+        redo
+      end
+    end
+    break if quit_flg
+  end
+
+  top_point, bottom_point = play_game(points)
+  display_result(top_point, bottom_point)
+end
