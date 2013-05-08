9e139704ad09c6841f5f8658224650e6bf36b53e
Tagaki Aki <a-tagaki@esm.co.jp>
2013-05-08 20:22:10 +0900

    Refactor
    引数でioを受け取るようにしたっっっ！

--- baseball.rb
+++ baseball.rb
@@ -7,48 +7,19 @@ class String
   end
 end
 
-def play_game(points)
-  top_point, bottom_point = [], []
+def play_game(io)
+  top      = {field: '表', point: []}
+  bottom   = {field: '裏', point: []}
 
-  points.each_with_index do |point, i|
-    team = i.even? ? top_point : bottom_point
-    team << point
-  end
-
-  [top_point.inject(:+), bottom_point.inject(:+)]
-end
-
-def display_result(top_point, bottom_point)
-  top_point    = 0 if top_point.nil?
-  bottom_point = 0 if bottom_point.nil?
-
-  puts "【試合結果】"
-  puts "#{top_point} 対 #{bottom_point} で"
-
-  if top_point > bottom_point
-    puts "表チームの勝ちです"
-  elsif top_point == bottom_point
-    puts "引き分けです！延長はありません！"
-  else
-    puts "裏チームの勝ちです"
-  end
-end
-
-if $0 == __FILE__
-  top      = {field: '表', point: 0}
-  bottom   = {field: '裏', point: 0}
-
-  points   = []
   quit_flg = false
-
   9.times do |i|
     [top, bottom].each do |team|
       print "#{i+1}回#{team[:field]}："
 
-      inputted = gets.chomp
+      inputted = io.gets.chomp
 
       if inputted.numeral?
-        points << inputted.to_i
+        team[:point] << inputted.to_i
       elsif inputted == 'quit'
         quit_flg = true
         break
@@ -60,6 +31,25 @@ if $0 == __FILE__
     break if quit_flg
   end
 
-  top_point, bottom_point = play_game(points)
+  [top[:point], bottom[:point]]
+end
+
+def display_result(top_point, bottom_point)
+  top, bottom = [top_point.inject(0, :+), bottom_point.inject(0, :+)]
+
+  puts "【試合結果】"
+  puts "#{top} 対 #{bottom} で"
+
+  if top > bottom
+    puts "表チームの勝ちです"
+  elsif top == bottom
+    puts "引き分けです！延長はありません！"
+  else
+    puts "裏チームの勝ちです"
+  end
+end
+
+if $0 == __FILE__
+  top_point, bottom_point = play_game(STDIN)
   display_result(top_point, bottom_point)
 end
--- baseball_spec.rb
+++ baseball_spec.rb
@@ -1,14 +1,14 @@
 $LOAD_PATH << File.dirname(__FILE__)
 require 'baseball'
+require 'stringio'
 
 describe 'baseball' do
-  let!(:score) { [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2] }
-
   specify do
+    score = StringIO.new("1\n2\n1\n2\n1\n2\n1\n2\n1\n2\n1\n2\n1\n2\n1\n2\n1\n2")
     top_point, bottom_point = play_game(score)
 
-    top_point.should == 9
-    bottom_point.should == 18
+    top_point.should == [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    bottom_point.should == [2, 2, 2, 2, 2, 2, 2, 2, 2]
   end
 end
 
