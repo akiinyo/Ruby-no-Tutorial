258264bab4307f297cd0e8ca7177ae049ce3f48f
Tagaki Aki <a-tagaki@esm.co.jp>
2013-06-08 15:56:57 +0900

    Fix spec

--- numer0n_spec.rb
+++ numer0n_spec.rb
@@ -1,20 +1,23 @@
 $LOAD_PATH << File.dirname(__FILE__)
 require 'numer0n'
 
-describe 'judge' do
-  let(:number) { ['6', '0', '2'] }
+describe Numer0n do
+  let(:numer0n) { Numer0n.new }
 
-  subject { judge(number, call) }
+  describe 'judge' do
+    before  { numer0n.number = %w(6, 0, 2) }
+    subject { numer0n.judge(call) }
 
-  context '1EAT-1BITE' do
-    let!(:call) { ['0', '3', '2'] }
+    context '1EAT-1BITE' do
+      let!(:call) { %w(0, 3, 2) }
 
-    it { should == [1, 1] }
-  end
+      it { should == [1, 1] }
+    end
 
-  context '3EAT' do
-    let!(:call) { ['6', '0', '2'] }
+    context '3EAT' do
+      let!(:call) { %w(6, 0, 2) }
 
-    it { should == [3, 0] }
+      it { should == [3, 0] }
+    end
   end
 end
