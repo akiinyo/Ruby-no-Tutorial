74d3aca29f9cd454dd4d2f72c41a73e5d422dbbd
Tagaki Aki <a-tagaki@esm.co.jp>
2013-06-16 23:41:57 +0900

    Add validation

--- numer0n.rb
+++ numer0n.rb
@@ -38,25 +38,36 @@ end
 if $0 == __FILE__
   numer0n = Numer0n.new
 
+  def valid?(call)
+    return false unless call.size == Numer0n::DIGIT
+
+    call.size == call.uniq.size
+  end
+
   while !numer0n.perfect?
     print 'コールする3桁の番号を入力してください：'
     call = gets.chomp.split('')
-    numer0n.judge(call)
-    puts numer0n.display_result
+    if valid?(call)
+      numer0n.judge(call)
+      puts numer0n.display_result
 
-    if numer0n.perfect?
-      puts "#{numer0n.count}ターンかかりました。"
-      puts '練習を続けますか？続ける場合は1を、終了する場合には2を入力してください。'
+      if numer0n.perfect?
+        puts "#{numer0n.count}ターンかかりました。"
+        puts '練習を続けますか？続ける場合は1を、終了する場合には2を入力してください。'
 
-      case gets.chomp
-      when '1'
-        numer0n = Numer0n.new
-        puts '新しい番号を作成しました。'
-      when '2'
-        puts '終了します。おつかれさまでした。'
-      else
-        puts 'ぺろーん。'
+        case gets.chomp
+        when '1'
+          numer0n = Numer0n.new
+          puts '新しい番号を作成しました。'
+        when '2'
+          puts '終了します。おつかれさまでした。'
+        else
+          puts 'ぺろーん。'
+        end
       end
+
+    else
+      puts '( ∵)コールできるのは *重複のない3桁* の番号だけです(∵ )'
     end
   end
 end
