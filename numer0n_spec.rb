88226eac61763c2d576f57e928daaccaac0b9e77
Tagaki Aki <a-tagaki@esm.co.jp>
2013-06-27 19:31:10 +0900

    Fix wrong syntax

--- numer0n_spec.rb
+++ numer0n_spec.rb
@@ -2,18 +2,18 @@ $LOAD_PATH << File.dirname(__FILE__)
 require 'numer0n'
 
 describe Numer0n do
-  let(:numer0n) { Numer0n.new(%w(6, 0, 2)) }
+  let(:numer0n) { Numer0n.new(%w(6 0 2)) }
 
   before { numer0n.judge(call) }
 
   context '1EAT-1BITE' do
-    let!(:call) { %w(0, 3, 2) }
+    let!(:call) { %w(0 3 2) }
 
     specify { numer0n.display_result.should == '1EAT-1BITE' }
   end
 
   context '3EAT' do
-    let!(:call) { %w(6, 0, 2) }
+    let!(:call) { %w(6 0 2) }
 
     specify { numer0n.display_result.should == '3EAT-0BITE' }
   end
