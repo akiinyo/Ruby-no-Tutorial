aeac2abf25e25b1b6fd3cd416ab3374cecbc35b2
Tagaki Aki <a-tagaki@esm.co.jp>
2013-07-02 21:12:39 +0900

    Use center method

--- diamond.rb
+++ diamond.rb
@@ -11,3 +11,12 @@ midpoint.downto(1) do |i|
   print '*' * ((i * 2) - 1)
   puts ''
 end
+
+
+1.upto(midpoint - 1) do |i|
+  puts ('*' * ((i * 2) - 1)).center((midpoint * 2) - 1)
+end
+
+midpoint.downto(1) do |i|
+  puts ('*' * ((i * 2) - 1)).center((midpoint * 2) - 1)
+end
