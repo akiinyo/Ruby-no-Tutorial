8296e954505abb111e96c607f9fea367d3a0d314
Tagaki Aki <a-tagaki@esm.co.jp>
2013-07-11 22:26:18 +0900

    Convert arabic numerals into roman numerals

---
+++ roman.rb
@@ -0,0 +1,45 @@
+roman = {
+ 'I'    => 1,
+ 'II'   => 2,
+ 'III'  => 3,
+ 'IV'   => 4,
+ 'V'    => 5,
+ 'VI'   => 6,
+ 'VII'  => 7,
+ 'VIII' => 8,
+ 'IX'   => 9,
+ 'X'    => 10,
+ 'XX'   => 20,
+ 'XXX'  => 30,
+ 'XL'   => 40,
+ 'L'    => 50,
+ 'LX'   => 60,
+ 'LXX'  => 70,
+ 'LXXX' => 80,
+ 'XC'   => 90,
+ 'C'    => 100,
+ 'CC'   => 200,
+ 'CCC'  => 300,
+ 'CD'   => 400,
+ 'D'    => 500,
+ 'DC'   => 600,
+ 'DCC'  => 700,
+ 'DCCC' => 800,
+ 'CM'   => 900,
+ 'M'    => 1000,
+ 'MM'   => 2000,
+ 'MMM'  => 3000
+}
+
+number = ARGV[0].to_i
+
+if roman.has_value?(number)
+  puts roman.key(number)
+else
+  m = number / 1000
+  c = number % 1000 / 100
+  x = number % 100 / 10
+  i = number % 10
+
+  puts roman.key(m * 1000) + roman.key(c * 100) + roman.key(x * 10) + roman.key(i)
+end
