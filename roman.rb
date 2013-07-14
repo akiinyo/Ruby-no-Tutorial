c0670e03f43ea096723037d35ceddec64574c469
Tagaki Aki <a-tagaki@esm.co.jp>
2013-07-14 16:18:08 +0900

    More clean

--- roman.rb
+++ roman.rb
@@ -1,7 +1,7 @@
-roman_i = %W(#{} I II III IV V VI VII VIII IX)
-roman_x = %W(#{} X XX XXX XL L LX LXX LXXX XC)
-roman_c = %W(#{} C CC CCC CD D DC DCC DCCC CM)
 roman_m = %W(#{} M MM MMM)
+roman_c = %W(#{} C CC CCC CD D DC DCC DCCC CM)
+roman_x = %W(#{} X XX XXX XL L LX LXX LXXX XC)
+roman_i = %W(#{} I II III IV V VI VII VIII IX)
 
 def can_convert?(number)
   (1..3999).include? number
@@ -15,10 +15,13 @@ if can_convert?(number)
   x = number % 100 / 10
   i = number % 10
 
-  print roman_m[m]
-  print roman_c[c]
-  print roman_x[x]
-  puts roman_i[i]
+  roman = ''
+  roman << roman_m[m]
+  roman << roman_c[c]
+  roman << roman_x[x]
+  roman << roman_i[i]
+
+  puts roman
 else
   puts 'ローマ数字に変換できるのは1から3999までです！'
 end
