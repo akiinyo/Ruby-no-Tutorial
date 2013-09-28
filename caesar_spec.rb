b292fa9c9085138369bb7ac556c66c4c9f71844c
Tagaki Aki <a-tagaki@esm.co.jp>
2013-09-28 11:39:19 +0900

    Add test case

--- caesar_spec.rb
+++ caesar_spec.rb
@@ -2,9 +2,27 @@ $LOAD_PATH << File.dirname(__FILE__)
 require 'caesar'
 
 describe '#convert' do
-  let(:caesar) { Caesar.new(',igglwm') }
+  context '正しく解読できる場合' do
+    let(:caesar) { Caesar.new(',igglwm') }
 
-  specify do
-    caesar.convert('aki').should == 'akiinyo'
+    specify do
+      caesar.convert('aki').should == 'akiinyo'
+    end
+  end
+
+  context '暗号文に、変換できない文字が含まれている場合' do
+    let(:caesar) { Caesar.new(';@*&#$!?~+=|') }
+
+    specify do
+      caesar.convert('aki').should == '解読失敗！変換できない文字が含まれています。'
+    end
+  end
+
+  context '指定された文字列が、変換後の文章に含まれていない場合' do
+    let(:caesar) { Caesar.new('abcdefg') }
+
+    specify do
+      caesar.convert('aki').should == '解読失敗！「aki」は含まれていません。'
+    end
   end
 end
