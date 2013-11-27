6d4f41c1a18af02e2325bf4c52e701528773c8cc
Tagaki Aki <a-tagaki@esm.co.jp>
2013-11-27 20:58:25 +0900

    Add red test case

--- next_prime_spec.rb
+++ next_prime_spec.rb
@@ -6,5 +6,14 @@ describe 'next_prime' do
     next_prime(10).should == 11
     next_prime(11).should == 13
     next_prime(44).should == 47
+    next_prime(0).should  == 2
+  end
+end
+
+describe 'prime?' do
+  specify do
+    prime?(-3).should be_false
+    prime?(1).should  be_false
+    prime?(2).should  be_true
   end
 end
